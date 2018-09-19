a83e1157802f117b886975c5bdab00a4422b6f2c
Bobby Boyd <bboyd2008@gmail.com>
2018-09-18 23:57:49 -0700

    added questions array

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+// TODO need to append questions to #questions
+// TODO need to append guesses to #first-guesses and #second guesses
+
+$(document).on("ready", function() {
+  var questions = [
+    {
+      question: "What did Ygritte say to Jon Snow?",
+      guess1: "You know everything, Jon Snow.",
+      answer: "You know nothing, Jon Snow.",
+      guess3: "How do you not know that, Jon Snow.",
+      guess4: "You're an idiot, Jon Snow."
+    }
+    ];
+    
+
+    
+});
