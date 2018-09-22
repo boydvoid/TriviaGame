a7adbc7e19f7c53efee778aff061d511d39520f3
Bobby Boyd <bboyd2008@gmail.com>
2018-09-21 21:11:48 -0700

    almost donee

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,49 +6,83 @@ $(document).ready(function() {
     {
       question: "What did Ygritte say to Jon Snow?",
       guess1: "You know everything, Jon Snow.",
-      answer: "You know nothing, Jon Snow.",
+      guess2: "You know nothing, Jon Snow.",
       guess3: "How do you not know that, Jon Snow.",
-      guess4: "You're an idiot, Jon Snow."
+      guess4: "You're an idiot, Jon Snow.",
+      answer: "You know nothing, Jon Snow.",
+      answered: false
     },
     {
       number: 1,
-      question: "What did Ygritte say to Jon Snow?",
+      question: "2",
       guess1: "You know everything, Jon Snow.",
-      answer: "You know nothing, Jon Snow.",
+      guess2: "You know nothing, Jon Snow.",
       guess3: "How do you not know that, Jon Snow.",
-      guess4: "You're an idiot, Jon Snow."
+      guess4: "You're an idiot, Jon Snow.",
+      answer: "You know nothing, Jon Snow."
     },
     {
       number: 2,
-      question: "What did Ygritte say to Jon Snow?",
+      question: "3?",
       guess1: "You know everything, Jon Snow.",
-      answer: "You know nothing, Jon Snow.",
+      guess2: "You know nothing, Jon Snow.",
       guess3: "How do you not know that, Jon Snow.",
-      guess4: "You're an idiot, Jon Snow."
+      guess4: "You're an idiot, Jon Snow.",
+      answer: "You know nothing, Jon Snow."
     },
     {
       number: 3,
-      question: "What did Ygritte say to Jon Snow?",
+      question: "4?",
       guess1: "You know everything, Jon Snow.",
-      answer: "You know nothing, Jon Snow.",
+      guess2: "You know nothing, Jon Snow.",
       guess3: "How do you not know that, Jon Snow.",
-      guess4: "You're an idiot, Jon Snow."
+      guess4: "You're an idiot, Jon Snow.",
+      answer: "You know nothing, Jon Snow."
     }
   ];
-
-  appendQuestion();
+  let questionsDiv = $("#questions");
+  let guesses = $("#first-guesses");
+  let domTimer = $("#timer");
+  let appendedDiv = $("<h2>");
+  let button1 = $("<button>");
+  let button2 = $("<button>");
+  let button3 = $("<button>");
+  let button4 = $("<button>");
+  let startBtn = $("<button>");
+  let timer = 30;
+  let index = 0;
+  var setTimer = setInterval(function() {
+    if (timer !== 0) {
+      timer--;
+      domTimer.text("TIME: " + timer);
+    } else {
+      console.log("clear");
+      clearInterval(setTimer);
+      $(questionsDiv).empty();
+      $(guesses).empty();
+      appendedDiv.text("The correct answer was: " + questions[index - 1].answer);
+      $(questionsDiv).append(appendedDiv);
+      setTimeout(appendGame, 3000);
+    }
+  }, 1000);
+  gameLoad();
   // functions --------------------
-  function appendQuestion() {
-    let index = Math.floor(Math.random() * questions.length);
-    let questionsDiv = $("#questions");
-    let firstGuesses = $("#first-guesses");
-    let secondGuesses = $("#second-guesses");
-    let appendedDiv = $("<h2>");
-    let button1 = $("<button>");
-    let button2 = $("<button>");
-    let button3 = $("<button>");
-    let button4 = $("<button>");
+  function gameLoad() {
+    startBtn.attr("id", "start-button");
+    startBtn.attr("class", "btn btn-secondary");
+    startBtn.text("START GAME");
+    $("#questions").append(startBtn);
+  }
 
+  //start button onclick
+  $(document).on("click", "#start-button", function() {
+    startBtn.attr("class", "hidden");
+    appendGame();
+  });
+
+  function appendGame() {
+    $(questionsDiv).empty();
+    $(guesses).empty();
     //append the question
     appendedDiv.attr("id", questions[index].number);
     appendedDiv.text(questions[index].question);
@@ -59,14 +93,34 @@ $(document).ready(function() {
     button2.attr("id", "guess2");
     button3.attr("id", "guess3");
     button4.attr("id", "guess4");
+    button1.attr("class", "btn btn-primary guesses");
+    button2.attr("class", "btn btn-primary guesses");
+    button3.attr("class", "btn btn-primary guesses");
+    button4.attr("class", "btn btn-primary guesses");
     button1.text(questions[index].guess1);
-    button2.text(questions[index].answer);
+    button2.text(questions[index].guess2);
     button3.text(questions[index].guess3);
     button4.text(questions[index].guess4);
-    
-    firstGuesses.append(button1);
-    firstGuesses.append(button2);
-    secondGuesses.append(button3);
-    secondGuesses.append(button4);
+
+    guesses.append(button1);
+    guesses.append(button2);
+    guesses.append(button3);
+    guesses.append(button4);
+    index++;
   }
+
+  $(document).on("click", ".guesses", function() {
+    let btnText = $(this).text();
+
+    if (questions[index].answer === btnText) {
+      console.log("answer");
+    } else {
+      clearInterval(setInterval);
+      $(questionsDiv).empty();
+      $(guesses).empty();
+      appendedDiv.text("The correct answer was: " + questions[index - 1].answer);
+      $(questionsDiv).append(appendedDiv);
+      setTimeout(appendGame, 3000);
+    }
+  });
 });
